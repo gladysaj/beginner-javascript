fdee3924899125353bf984569b73159646d16613
Gladys Alvarez <glad.alvarez1@gmail.com>
2020-11-19 22:28:00 -0600

    types: null and undefined

--- playground/types.js
+++ playground/types.js
@@ -1,33 +1,40 @@
 /* eslint-disable */
-const name = 'gladys';
-const lastName = "alvarez";
-const wes = `bos`;
-const sentence = 'she\'s so cool'; // or use ""
-const sentence2 = `she's so "cool"`;
+// const name = 'gladys';
+// const lastName = "alvarez";
+// const wes = `bos`;
+// const sentence = 'she\'s so cool'; // or use ""
+// const sentence2 = `she's so "cool"`;
 
-const hello = "Hello my name is" + name; // inteporlation 
-const hi = `Hello, this is my ${name}`; // concatenation 
+// const hello = "Hello my name is" + name; // inteporlation 
+// const hi = `Hello, this is my ${name}`; // concatenation 
 
-const html = ` 
-<div>
-<h2> Hello world. My name is ${name}</h2>
-</div>
-`;
-document.body.innerHTML = html;
+// const html = ` 
+// <div>
+// <h2> Hello world. My name is ${name}</h2>
+// </div>
+// `;
+// document.body.innerHTML = html;
 
-// Numbers 
-const smarties = 20;
-const kids = 3;
-const eachKidGets = smarties / kids;
-const result = Math.floor(eachKidGets);
-console.log(result);
+// // Numbers 
+// const smarties = 20;
+// const kids = 3;
+// const eachKidGets = smarties / kids;
+// const result = Math.floor(eachKidGets);
+// console.log(result);
 
-const smartiesLeft = smarties % kids; // this will be the modulus op with 2 left
+// const smartiesLeft = smarties % kids; // this will be the modulus op with 2 left
 
-// Objects 
-const person = {
-  name: 'Gladys',
-  age: 27,
-};
+// // Objects 
+// const person = {
+//   name: 'Gladys',
+//   age: 27,
+// };
 
-person.name;
+// person.name;
+
+// // Null and undefined 
+// let dog; // undefined 
+// console.log(dog);
+
+// let somethingUndefined; // no value set to it 
+// const somethingNull =  null // value equals to nothing 
