e840fc0fee344820245a55d84970854cf38e84d7
Gladys Alvarez <glad.alvarez1@gmail.com>
2020-11-19 23:05:19 -0600

    functions: custom

---
+++ playground/custom-functions/cf.js
@@ -0,0 +1,14 @@
+/* eslint-disable*/
+
+// Function definition
+function calculateBill() {
+// This is the function body
+// console.log('Running calculate bill');
+const total = 100 * 1.13;
+return total;
+};
+
+// Function called or running
+const myTotal = Math.round(calculateBill());
+console.log(`Your total is: ${myTotal}`);
+console.log(`Your total is: ${calculateBill()}`); // another option 
