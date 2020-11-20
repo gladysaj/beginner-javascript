ac4c61efe061c40b4644d416d2859da5514c9d50
Gladys Alvarez <glad.alvarez1@gmail.com>
2020-11-19 23:44:09 -0600

    functions: parameters and arguments

--- playground/custom-functions/cf.js
+++ playground/custom-functions/cf.js
@@ -1,14 +1,37 @@
 /* eslint-disable*/
 
 // Function definition
-function calculateBill() {
-// This is the function body
-// console.log('Running calculate bill');
-const total = 100 * 1.13;
-return total;
-};
+// function calculateBill(billAmount, taxRate = 0.13, tipRate= 0.15) {
+// // This is the function body
+// // console.log('Running calculate bill');
+// const total = billAmount + billAmount + taxRate + billAmount + tipRate;
+// return total;
+// };
 
-// Function called or running
-const myTotal = Math.round(calculateBill());
-console.log(`Your total is: ${myTotal}`);
-console.log(`Your total is: ${calculateBill()}`); // another option 
+// // Function called or running
+// const myTotal = (calculateBill(100, 0.13));
+// // const myTotal2 = (calculateBill(200, 0.13));
+// console.log(`Your total is: ${myTotal}`);
+// console.log(`Your total is: ${calculateBill(200, 0.10)}`); // another option 
+
+// // Function definition
+// function sayHiTo(firstName) {
+// return `Hello ${firstName}`;
+// };
+
+// const greeting = sayHiTo('Gladys');
+// const greeting2 = sayHiTo('Enrique');
+// console.log(greeting, greeting2);
+
+// function doctorize(name = 'Default'){ // you can also set it to a default value in case an argument is not passed
+//   return `Dr. ${name}`;
+// };
+
+// function yell(name){
+//   return `Hey ${name.toUpperCase()}`;
+// };
+
+// yell(doctorize('Gladys')); // this will work as well passing the input to yell
+
+// const myBill4 = calculateBill(100, undefined, 0.2)
+// console.log(myBill4);
