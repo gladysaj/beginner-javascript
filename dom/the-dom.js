46550187a5d358731de1b7cfab7dbd36f4c87ca8
Gladys Alvarez <glad.alvarez1@gmail.com>
2020-11-21 00:26:50 -0600

    dom: element properties and methods

--- dom/the-dom.js
+++ dom/the-dom.js
@@ -11,3 +11,21 @@
 // console.log(items);
 // console.log(imgs);
 
+
+// const heading = document.querySelectorAll('h2');
+// // display properties of the h2
+// console.dir(heading);
+// // display the value or input of the h2 - textContent is also used to set a value
+// console.log(heading.textContent);
+// // the same function as textContent but this one doesn't display for example the styling
+// console.log(heading.innerText);
+// // values inside the html 
+// console.log(heading.innerHTML);
+// // values including the html tags
+// console.log(heading.outerHTML);
+
+// const pizzaList = document.querySelector('.pizza');
+// console.log(pizzaList.textContent)
+
+// // pizzaList.textContent = `${pizzaList.textContent} pizza-emoji`; 
+// pizzaList.insertAdjacentText('beforeend', 'pizza-emoji');
