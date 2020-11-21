08cee647da6d834f7c0d256caea8c58581860817
Gladys Alvarez <glad.alvarez1@gmail.com>
2020-11-20 22:39:46 -0600

    debugging tools

--- debugging/debugging.js
+++ debugging/debugging.js
@@ -1,30 +1,54 @@
 const people = [
-  { name: 'Wes', cool: true, country: 'Canada' },
-  { name: 'Scott', cool: true, country: 'Merica' },
+  { name: 'Gladys', cool: true, country: 'Mexico' },
+  { name: 'Scott', cool: true, country: 'France' },
   { name: 'Snickers', cool: false, country: 'Dog Country' },
 ];
 
 people.forEach((person, index) => {
+  // if the console is open this will pause JS from running, like a breakpoint will show the sources tab
+  // debugger;
   console.log(person.name);
 });
 
 // Console Methods
+// console.info('HI');
+// console.error('Dumb');
+// console.warn('Test');
+// console.group();
+// console.groupEnd();
+// console.groupCollapsed();
 
-// Callstack
+// Callstack or stack trace
+// This is to read the errors on the console
 
-// Grabbing Elements
+// GrabbingElements
+// inspect an element on the console and then add $0 this will show you the element
+// if the page has jquery you can add $$('p') to select all the elements for example within that class 
 
 // Breakpoints
 
 // Scope
 
 // Network Requests
+// open the console and go to Network you can see all the files needed 
 
 // Break On Attribute
+// on the console select an element then right click and select break on->attribute modifications
+// this will show a paused in debugger
 
 // Some Setup Code
 
+// To console log one collapsable group (console.groupCollapsed)
+function doALotOfStuff(){
+  console.group('Doing some stuff');
+  console.info('HI');
+  console.error('Dumb');
+  console.warn('Test');
+  console.groupEnd('Doing some stuff');
+}
+
 function doctorize(name) {
+  console.count('running Doctorize');
   return `Dr. ${name}`;
 }
 
@@ -38,6 +62,7 @@ function go() {
   console.log(name);
 }
 
+// Make me bigger button!
 const button = document.querySelector('.bigger');
 button.addEventListener('click', function(e) {
   const newFontSize =
