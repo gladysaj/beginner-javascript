f60caf8c1d7faaf933d325465040dfdeec846b93
Gladys Alvarez <glad.alvarez1@gmail.com>
2020-11-21 00:09:53 -0600

    dom: selecting elements

---
+++ dom/the-dom.js
@@ -0,0 +1,13 @@
+// in case you added the script on the head in the html 
+// document.addEventListener('DOMContentLoaded', init)
+
+// const p = document.querySelector('p');
+// const divs = document.querySelectorAll('div');
+// const items = document.querySelectorAll('.item');
+// const imgs = document.querySelectorAll('.item img');
+// // this will return a node list on the console
+// console.log(p);
+// console.log(divs);
+// console.log(items);
+// console.log(imgs);
+
