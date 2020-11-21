ccab1754aa55ade1ced37753f4826cf5d0900150
Gladys Alvarez <glad.alvarez1@gmail.com>
2020-11-20 23:26:24 -0600

    hoisting

---
+++ playground/hoisting.js
@@ -0,0 +1,20 @@
+// Hoisting = access functions and variables before they are being created
+// JavaScript rearrange the functions and moves them to the top
+
+// Variable hoisting
+// console.log(age);
+// var age = 10;
+
+// sayHi();
+
+// function sayHi(){
+//   console.log('Hey');
+// };
+
+// function sum(a,b){
+//   return a + b;
+// };
+
+// console.log(sum(4,5));
+
+// const sum = (a,b) => a + b; (arrow practice)
